dd34d42432d989b79e6af0f11a0c8eea6b2d7c52
ABetancourt03 <angelob2207@gmail.com>
2025-04-01 01:55:21 -0400

    Add datagen providers

--- src/main/java/com/abetancourt03/angeloshammers/AngelosHammersDataGenerator.java
+++ src/main/java/com/abetancourt03/angeloshammers/AngelosHammersDataGenerator.java
@@ -1,11 +1,16 @@
 package com.abetancourt03.angeloshammers;
 
+import com.abetancourt03.angeloshammers.datagen.ModModelProvider;
+import com.abetancourt03.angeloshammers.datagen.ModRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class AngelosHammersDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
