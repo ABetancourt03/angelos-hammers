b547ed898ae8d6f69e99595aec9ac05560e8c96c
ABetancourt03 <angelob2207@gmail.com>
2025-04-01 13:13:05 -0400

    Add Data generation for recipes and recipe advancements

--- src/main/java/com/abetancourt03/angeloshammers/datagen/ModRecipeProvider.java
+++ src/main/java/com/abetancourt03/angeloshammers/datagen/ModRecipeProvider.java
@@ -1,8 +1,16 @@
 package com.abetancourt03.angeloshammers.datagen;
 
+import com.abetancourt03.angeloshammers.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.RecipeProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.data.server.recipe.SmithingTransformRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.item.SmithingTemplateItem;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.tag.ItemTags;
 
 import java.util.function.Consumer;
 
@@ -13,6 +21,54 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public void generate(Consumer<RecipeJsonProvider> consumer) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.WODDEN_HAMMER, 1)
+                .pattern("###")
+                .pattern("#S#")
+                .pattern(" S ")
+                .input('#', ItemTags.PLANKS)
+                .input('S', Items.STICK)
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .criterion(hasItem(Items.OAK_PLANKS), conditionsFromItem(Items.OAK_PLANKS))
+                .offerTo(consumer);
 
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.STONE_HAMMER, 1)
+                .pattern("###")
+                .pattern("#S#")
+                .pattern(" S ")
+                .input('#', ItemTags.STONE_TOOL_MATERIALS)
+                .input('S', Items.STICK)
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .criterion(hasItem(Items.COBBLESTONE), conditionsFromItem(Items.COBBLESTONE))
+                .offerTo(consumer);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.GOLDEN_HAMMER, 1)
+                .pattern("###")
+                .pattern("#S#")
+                .pattern(" S ")
+                .input('#', Items.GOLD_INGOT)
+                .input('S', Items.STICK)
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .criterion(hasItem(Items.GOLD_INGOT), conditionsFromItem(Items.GOLD_INGOT))
+                .offerTo(consumer);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.IRON_HAMMER, 1)
+                .pattern("###")
+                .pattern("#S#")
+                .pattern(" S ")
+                .input('#', Items.IRON_INGOT)
+                .input('S', Items.STICK)
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
+                .offerTo(consumer);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.DIAMOND_HAMMER, 1)
+                .pattern("###")
+                .pattern("#S#")
+                .pattern(" S ")
+                .input('#', Items.DIAMOND)
+                .input('S', Items.STICK)
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .criterion(hasItem(Items.DIAMOND), conditionsFromItem(Items.DIAMOND))
+                .offerTo(consumer);
     }
 }
